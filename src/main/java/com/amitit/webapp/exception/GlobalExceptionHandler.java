01f7aaa31890a5055bba97167a7e8a3ff2613826
ParmeshvarKate <parmeshvarkate12@gmail.com>
2025-06-01 10:54:42 +0530

    changes done as per feedback

--- src/main/java/com/amitit/webapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/amitit/webapp/exception/GlobalExceptionHandler.java
@@ -1,7 +1,5 @@
 package com.amitit.webapp.exception;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -9,13 +7,8 @@ import org.springframework.web.bind.annotation.ExceptionHandler;
 @ControllerAdvice
 public class GlobalExceptionHandler {
 	@ExceptionHandler(CourseServiceException.class)
-public ResponseEntity handleException(CourseServiceException exception)
-{
-	return new ResponseEntity(exception.getMessage(),exception.getHttpStatus());
-}
-
-	
+	public ResponseEntity handleCourseServiceException(CourseServiceException courseexception) {
+		return new ResponseEntity(courseexception.getMessage(), courseexception.getHttpStatus());
+	}
 
 }
-
-
