73ce7ac856e40775ce3173cf0cbcafc90a226b75
Snehasute20 <snehasute0320@gmail.com>
2025-06-23 12:07:55 +0530

    Enquiry Api

---
+++ src/main/java/com/amitit/webapp/entity/Enquiry.java
@@ -0,0 +1,26 @@
+package com.amitit.webapp.entity;
+
+import java.time.LocalDate;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Enquiry {
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int id;
+	private String name;
+	private String email;
+	private String contact;
+	private String messege;
+	private LocalDate enquiryDate;
+	private String Course;
+
+}
