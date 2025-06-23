5b664132b2f92bfb6287c643dce5e738822a1c4e
RahulChaudhari05 <rahulchaudhari3338@gmail.com>
2025-06-23 23:33:57 +0530

    Changes made as per PR review

--- src/main/java/com/amitit/webapp/exception/GlobalExceptionHandler.java
+++ src/main/java/com/amitit/webapp/exception/GlobalExceptionHandler.java
@@ -1,8 +1,11 @@
 package com.amitit.webapp.exception;
 
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
+
+import com.amitit.webapp.constant.UserProfileConstants;
 
 @ControllerAdvice
 public class GlobalExceptionHandler {
@@ -11,4 +14,14 @@ public class GlobalExceptionHandler {
 		return new ResponseEntity(syllabusServiceException.getExMessage(), syllabusServiceException.getHttpStatus());
 	}
 
+	
+	@ExceptionHandler(UserServiceException.class)
+    public ResponseEntity<String> handleUserServiceException(UserServiceException ex) {
+        return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(ex.getMessage());
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<String> handleGeneralException(Exception ex) {
+        return ResponseEntity.status(HttpStatus.INTERNAL_SERVER_ERROR).body(UserProfileConstants.UNEXPECTED_ERROR);
+    }
 }
